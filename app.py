0a8eb4b84874d935b497615703f7fe95c7587ee5
Manfredi Corradino <cmanfredi@hixoltage.noise>
2018-09-10 20:42:14 -0700

    added a comment to app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-
+#Inserted a comment here, Freddy
 @app.route('/goodbye_world')
 def goodbye_world():
     return 'goodbye, World!'
